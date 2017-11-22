7210a828981e4c8c251e6f08af20c37fa092076e
zahrank21 <zahrank@gmail.com>
2017-11-22 07:42:36 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(arr)
   counter = 0
   result = []
   while i < arr.length
-    block_given? 
-    result << yield(array[i]) 
+    block_given?
+    result << yield(array[i])
     i += 1
   end
 result
